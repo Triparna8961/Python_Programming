b6b45cf96f4ba99dda7eda8d2876162114201d66
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2024-07-20 10:49:46 +0530

    Add files via upload

---
+++ Arithmatic_operators.py
@@ -0,0 +1,24 @@
+#Addition
+print(5+4)
+
+#Subtraction
+print(5-4)
+
+#Multiplication
+print(5*4)
+
+#division
+print(5/4)
+
+#floor division(only takes berfor the decimal)
+print(15//4) 
+
+#moduls
+print(15%4)
+
+#exponention
+print(2**3)#(2^3)
+
+
+#precedence
+#**>*,//,/>+,-
