714e70bbc321a965dd8775ad2d11b98750bf70f3
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2024-07-20 10:38:36 +0530

    Add files via upload

---
+++ Escape_Character.py
@@ -0,0 +1,10 @@
+#"\" used as escape key 
+print("I\'ll call you later")
+
+print("We upload our file in \"GITHUB\" and then easily share the link")
+
+#"\n","\t","\r" also used as escape character
+print("We are practicing\n") #for new line
+print("we\tare\tpracticing") #worked as a tab
+print("no_benefits_of_study\rHuge_benefits_") #replace the portion
+#In the above line "no_benefits" has 11 characters but "huge_benefits_" has 14 characters. It will replace those 14 character.
