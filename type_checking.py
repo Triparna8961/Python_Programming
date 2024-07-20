5431362c6fe89c7e6eb72547c02882f3636f069d
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2024-07-20 10:11:03 +0530

    Add files via upload

---
+++ type_checking.py
@@ -0,0 +1,23 @@
+age= 23
+type(age)
+weight=45.68
+type(weight)
+name="Triparna"
+type(name)
+is_girl=True
+type(is_girl)
+list=[1,84.6,"mango"]
+type(list)
+dict1={1:"Triparna",2:"23","weight":50.9}
+type(dict1)
+tup=(1,2,3,47,8.9,"Sarkar")
+type(tup)
+set={1,2,3,5.9,"Sarkar"}
+type(set)
+com=1+2j
+type(com)
+byte=bytes([65,66,92])
+type(byte)
+b=None
+type(b)
+'''To command multiple line and to print all line at once without using print again and again'''
