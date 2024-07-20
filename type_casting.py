a66955ed768e578d9d317632e57da3fe3530a2c0
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2024-07-20 10:24:45 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+#taking input as a string
+str=input("Enter the number")
+print(str)
+type(str)
+
+#TYPE CASTING
+
+#taking input as a integer 
+num=int(input("Enter the number"))
+print(num)
+type(num)
+
+#taking input as a float
+fnum=float(input("Enter the number"))
+print(fnum)
+type(fnum)
