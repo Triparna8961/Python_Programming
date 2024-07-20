5d16f23ae1b9e9eb33d1cb56e8bf5a6f4942acc6
Triparna8961 <109430584+Triparna8961@users.noreply.github.com>
2024-07-20 11:05:27 +0530

    Add files via upload

---
+++ comparison_operator.py
@@ -0,0 +1,26 @@
+a="Who is the king of a jungle?"
+b="lion"
+#calculating the length of the strings
+c=len(a)
+d=len(b)
+print(c,d)
+if(c>d):
+    print("Who is the king of a jungle?")
+if(c<d):
+    print("lion")
+if(c==d):
+    print("Same")
+
+m=True
+n=False
+if(m==n):
+    print("Same")
+if(m!=n):
+    print("Different")
+
+x=156
+y=-156
+if(x>=y):
+    print(x)
+if(x<=y):
+    print(y)
